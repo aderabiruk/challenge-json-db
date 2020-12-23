f610cf62482341ee64ef4eaf65dc06000a9cc8e4
Biruk Adera <aderabiruk@gmail.com>
2020-12-24 02:44:12 +0300

    feat: GET /:student/:propert endpoint added

--- api.js
+++ api.js
@@ -1,7 +1,49 @@
+const fileHelpers = require("./helpers/file.helpers")
+const storageHelpers = require("./helpers/storage.helpers")
+
 module.exports = {
-  getHealth
+    getHealth,
+
+    // Student Property
+    getStudentProperty,
+    saveStudentProperty,
+    deleteStudentProperty
 }
 
 async function getHealth (req, res, next) {
-  res.json({ success: true })
+    res.json({ success: true })
+}
+
+async function getStudentProperty (req, res, next) {
+    // Get Student Storage Filepath
+    const filepath = fileHelpers.getStudentStorageFilepath(req.params.student)
+
+    if (storageHelpers.exists(filepath)) {
+        let path = req.params[0].split("/")
+
+        // Read and Parse Student Record
+        let studentRecord = storageHelpers.read(filepath)
+        let result = storageHelpers.parseObject(studentRecord, path)
+        if (result) {
+            res.status(200).json(result)
+        }
+        else {
+            res.status(404).json({
+                message: "Student property not found!"
+            })
+        }
+    }
+    else {
+        res.status(404).json({
+            message: "Student record not found!"
+        })
+    }
+}
+
+async function saveStudentProperty (req, res, next) {
+    res.json({ success: true })
+}
+
+async function deleteStudentProperty (req, res, next) {
+    res.json({ success: true })
 }
